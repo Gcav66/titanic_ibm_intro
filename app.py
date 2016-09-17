b958db2ab8d9663ed0037942ced679cc30cfd5ef
Gus Cavanaugh <Gus Cavanaugh>
2016-09-16 21:51:34 -0400

    added summary stats

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template, send_file
 import pandas as pd
 import os
-from model import get_sample_data
+from model import get_sample_data, get_summary_stats
 
 def create_app():
     app=Flask(__name__)
@@ -14,19 +14,22 @@ app = create_app()
 def table():
     template = 'table.html'
     data_df = get_sample_data('train.csv')
-    return render_template(template, data=data_df.to_html(index=False))
+    stats_df = get_summary_stats('train.csv')
+    return render_template(template, data=data_df.to_html(index=False), summary=stats_df.to_html(index=False))
 
 @app.route("/table-css", methods=['GET', 'POST'])
 def table_css():
     template = 'table_css.html'
     data_df = get_sample_data('train.csv')
-    return render_template(template, data=data_df.to_html(index=False))
+    stats_df = get_summary_stats('train.csv')
+    return render_template(template, data=data_df.to_html(index=False), summary=stats_df.to_html(index=False))
 
 @app.route("/table-css-custom", methods=['GET', 'POST'])
 def table_css_custom():
     template = 'table_css_custom.html'
     data_df = get_sample_data('train.csv')
-    return render_template(template, data=data_df.to_html(index=False))
+    stats_df = get_summary_stats('train.csv')
+    return render_template(template, data=data_df.to_html(index=False), summary=stats_df.to_html(index=False))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
--- model.py
+++ model.py
@@ -7,9 +7,17 @@ Created on Fri Sep 16 20:40:22 2016
 
 import pandas as pd
 
+
 def get_sample_data(input_file):
 	df = pd.read_csv(input_file)
 	df_sample = df.head()
 	return df_sample
+
+def get_summary_stats(input_file):
+	df = pd.read_csv(input_file)
+	df_stats = df.describe()
+	return df_stats
+
+
 
 
